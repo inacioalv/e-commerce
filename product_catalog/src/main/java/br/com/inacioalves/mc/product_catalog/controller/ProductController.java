92cebccc476c2a97e8ea96485d679de17f22cc49
inacioalv <51245154+inacioalv@users.noreply.github.com>
2021-11-01 16:17:54 -0300

    db && Swagger

--- product_catalog/src/main/java/br/com/inacioalves/mc/product_catalog/controller/ProductController.java
+++ product_catalog/src/main/java/br/com/inacioalves/mc/product_catalog/controller/ProductController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,9 +24,13 @@ import br.com.inacioalves.mc.product_catalog.dto.ProductDto;
 import br.com.inacioalves.mc.product_catalog.exeption.objectNotFoundException;
 import br.com.inacioalves.mc.product_catalog.responses.Response;
 import br.com.inacioalves.mc.product_catalog.service.ProductService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("/product")
+@Api(value = "Api Rest plant")
+@CrossOrigin(origins = "*")
 public class ProductController {
 	
 	private ProductService service;
@@ -37,6 +42,7 @@ public class ProductController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
+	@ApiOperation(value = "Create product")
 	public ResponseEntity<ProductDto> createProduct(@Valid @RequestBody ProductDto productDto,BindingResult result) {
 		Response<ProductDto> response = new Response<ProductDto>();
 		
@@ -56,11 +62,13 @@ public class ProductController {
 	}
 	
 	@GetMapping("/all")
+	@ApiOperation(value = "List product")
 	public List<ProductDto> listAll(){
 		return service.listAll();
 	}
 	
 	@GetMapping("/{id}")
+	@ApiOperation(value = "Get By ID")
 	public ProductDto findById(@PathVariable Long id) throws objectNotFoundException {
 		 return service.findById(id);
 	 }
@@ -68,6 +76,7 @@ public class ProductController {
 	 
 	 @PutMapping("update/{id}")
 	 @ResponseStatus(HttpStatus.OK)
+	 @ApiOperation(value = "Update product")
 	 public ResponseEntity<Void> UpdateProduct(@RequestBody ProductDto plantDto,@PathVariable Long id){
 		 	plantDto.setId(id);
 			service.updateById(plantDto,id);
@@ -76,6 +85,7 @@ public class ProductController {
 	 
 	 @DeleteMapping("/{id}")
 	 @ResponseStatus(code = HttpStatus.NO_CONTENT)
+	 @ApiOperation(value = "delete product")
 	 public ResponseEntity<Void>  deleteById(@PathVariable  Long id) throws objectNotFoundException {
 		 service.deleteById(id);
 		 return ResponseEntity.noContent().build();
