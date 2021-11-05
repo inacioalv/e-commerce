d2dcd6ff58ff95d81fb2029ecaedb61adc440046
inacioalv <51245154+inacioalv@users.noreply.github.com>
2021-11-05 14:51:04 -0300

    Config DB

--- user_service/src/main/java/br/com/inacioalves/mc/user/model/User.java
+++ user_service/src/main/java/br/com/inacioalves/mc/user/model/User.java
@@ -1,10 +1,12 @@
 package br.com.inacioalves.mc.user.model;
 
+import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,10 +18,12 @@ import lombok.NoArgsConstructor;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "TB_USER")
 public class User {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_user")
 	private Long id;
 	private String first_name;
 	private String last_name;
